afd505a71ae201768e93c6c29ba6d2ac3bdd1561
bubai-009 <shivachatterjee643@gmail.com>
2024-07-16 03:50:39 +0530

    Replace bcrypt with bcryptjs

--- backend/controlers/userController.js
+++ backend/controlers/userController.js
@@ -58,7 +58,7 @@ const registerUser = async (req, res) => {
     }
 
     // hashing user password
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(password, salt);
 
     const newUser = new userModel({
