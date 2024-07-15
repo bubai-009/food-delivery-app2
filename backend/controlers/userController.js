689244aa276e99e5b393a75eddf368449ce55466
bubai-009 <shivachatterjee643@gmail.com>
2024-07-16 02:13:50 +0530

    Replace bcrypt with bcryptjs

--- backend/controlers/userController.js
+++ backend/controlers/userController.js
@@ -1,10 +1,12 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import validator from "validator";
 
 //login user
 const loginUser = async (req, res) => {
+  const bcrypt = require("bcryptjs");
   const { email, password } = req.body;
   try {
     const user = await userModel.findOne({ email });
