0a35f44a3f19f0cca5dbccac27f9e9c2589505e8
Shiva chatterjee <159185245+bubai-009@users.noreply.github.com>
2024-07-29 01:01:48 +0530

    Update orderController.js

--- backend/controlers/orderController.js
+++ backend/controlers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://food-delivery-frontend-zrfy.onrender.com";
+  const frontend_url = "https://food-delivery-frontend-o3cj.onrender.com";
 
   try {
     const newOrder = new orderModel({
